6df0e54602153824dfe1c7d7e793c01fffdb7917
dhadesugur <madhus.dhadesugur@gmail.com>
2025-10-04 09:39:01 +0530

    Added missing Research Papers
    Added the papers and linked them on VS Code
    
      git config --madhu.dhadesugur@gmail.com
      git config --@dhadesugur

--- src/Data/ResearchProjectsData.jsx
+++ src/Data/ResearchProjectsData.jsx
@@ -5,27 +5,33 @@ export const ResearchData = [
             {
                 title: "Understanding and analysing the role of social media and its algorithm in affecting body image amongst GBMSM.",
                 author: "Abhishek Lonari",
-                url: "/assets/researchPapers/abhishek.pdf"
+                url: "/assets/researchPapers/abhishek.pdf",
+                abstract: " "
             },
             {
-                title: "Lorem Ipsum",
-                author: "Lorem Ipsum",
-                url: ""
+                title: "Understanding the Role of Artificial Intelligence in User Experience & Consumer Behavior",
+                author: "Achala Athreya",
+                url: "/assets/researchPapers/Achala Athreya.pdf"
             },
             {
-                title: "Lorem Ipsum",
-                author: "Lorem Ipsum",
-                url: ""
+                title: "Interventions in the artisanal process",
+                author: "Aniket Patil, Shreyas Desai",
+                url: "/assets/researchPapers/Aniket Patil and Shreyas Desai.pdf"
             },
             {
-                title: "Lorem Ipsum",
-                author: "Lorem Ipsum",
-                url: ""
+                title: "Impact of Instagram Reels on the attention span of creative and analytic thinkers",
+                author: "HS Varshaa, Shreya K",
+                url: "/assets/researchPapers/HS Varshaa and Shreya K.pdf"
             },
             {
-                title: "Lorem Ipsum 1",
-                author: "Lorem Ipsum 2",
-                url: ""
+                title: "Understanding The Effect Of Different Audio Stimuli While Performing Some Logical and Creative Mental Tasks",
+                author: "Nishtha Rikhi, Samanvay Sahani",
+                url: "/assets/researchPapers/Nishtha Rikhi and Samanvay Sahani.pdf"
+            },
+            {
+                title: "Does the change in tempo affect the attention span?",
+                author: "Sharen Mary Charley",
+                url: "/assets/researchPapers/Sharen Mary Charley.pdf"
             }
         ]
     },
@@ -33,24 +39,19 @@ export const ResearchData = [
         sem: "Even sem 2022-2023",
         papers: [
             {
-                title: "Lorem Ipsum",
-                author: "Lorem Ipsum",
-                url: ""
+                title: "Realizing the significance of ESG (Environment, Social, and Governance), particularly the S aspect, how ESG can be embedded in an organization, and how ESG can influence an organization's culture and behavior",
+                author: "Anuvrat Sharma, Puneeta Kashyap",
+                url: "/assets/researchPapers/Anuvrat.pdf"
             },
             {
-                title: "Lorem Ipsum",
-                author: "Lorem Ipsum",
-                url: ""
+                title: "Understanding Gamification for Data Collection and the Role Played by Reward and Value in Game Systems",
+                author: "Saee Kerkar",
+                url: "/assets/researchPapers/Saee.pdf"
             },
             {
-                title: "Lorem Ipsum",
-                author: "Lorem Ipsum",
-                url: ""
-            },
-            {
-                title: "Lorem Ipsum",
-                author: "Lorem Ipsum",
-                url: ""
+                title: "The Impact of Colour Contrast on Visual Attention",
+                author: "HS Varshaa",
+                url: "/assets/researchPapers/Varshaa.pdf"
             }
         ]
     },
@@ -58,24 +59,64 @@ export const ResearchData = [
         sem: "Even sem 2022-2023",
         papers: [
             {
-                title: "Lorem Ipsum",
-                author: "Lorem Ipsum",
-                url: ""
+                title: "Early Research Inquiry: Consumer Awareness and Behaviour in the Millets Context",
+                author: "Akshita Purohit",
+                url: "/assets/researchPapers/akshita.pdf"
             },
             {
-                title: "Lorem Ipsum",
-                author: "Lorem Ipsum",
-                url: ""
+                title: "Empowering Wheat Farmers in Uttar Pradesh with FASAL : A Digital Revolution",
+                author: "Anuvrat Sharma",
+                url: "/assets/researchPapers/Anuvrat_2.pdf"
             },
             {
-                title: "Lorem Ipsum",
-                author: "Lorem Ipsum",
-                url: ""
+                title: "AI, chatGPT affecting critical thinking- Students and Teachers",
+                author: "Effie Sasha Fernandes, Samanvay Sahani",
+                url: "/assets/researchPapers/effiesamanvay.pdf"
             },
             {
-                title: "Lorem Ipsum",
-                author: "Lorem Ipsum",
-                url: ""
+                title: "Understanding and Addressing Body-Focused Repetitive Behaviours (BFRB): A Comprehensive Investigation",
+                author: "Esha P Raikar",
+                url: "/assets/researchPapers/esha.pdf"
+            },
+            {
+                title: "Connections Between Cognitive Biases, Error Management Theory, And User Experience Design",
+                author: "Heli Motanpotra",
+                url: "/assets/researchPapers/heli.pdf"
+            },
+            {
+                title: "Designing Ethical Solutions: Combating Dark Patterns with User-Centric Tools and Awareness",
+                author: "HS Varshaa",
+                url: "/assets/researchPapers/Varsha.pdf"
+            },
+            {
+                title: "Crafting User-Centric Innovation: Enhancing Focus on Emotional Support",
+                author: "Nishtha. R",
+                url: "/assets/researchPapers/Nishtha.pdf"
+            },
+            {
+                title: "Understanding the dark patterns in push notifications in games and its influence on user behavior",
+                author: "Praneetha Sali",
+                url:"/assets/researchPapers/Praneetha.pdf"
+            },
+            {
+                title: "From Trust to Skepticism: Unpacking Virtual Space Dynamics",
+                author: "Priti Dhongani",
+                url:"/assets/researchPapers/Priti.pdf"
+            },
+            {
+                title:"Understanding NPA– Borrowers’ Perspective",
+                author: "Puneeta Kashyap, Sushrut Baporikar",
+                url:"/assets/researchPapers/Puneeta Sushrut.pdf"
+            }, 
+            {
+                title: "Instant Gratification, Reshaping the Consumer Behavior in the Digital Age",
+                author: "Tanima Jajoo",
+                url:"/assets/researchPapers/Tanima.pdf"
+            },
+            {
+                title: "Understand User Behaviour Patterns Through Website Navigation Using Eye and Mouse Trackers",
+                author: "Vishnu S",
+                url:"/assets/researchPapers/Vishnu.pdf"
             }
         ]
     }
