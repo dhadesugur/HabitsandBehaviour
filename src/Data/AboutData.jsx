3a73f355cc89672d204f7b338855247f453a0a1e
dhadesugur <madhu.dhadesugur@gmail.com>
2025-10-06 15:57:59 +0530

    Edited About and Intro Page
    I have added brief intro about the Lab and wrote about the experiments in the first page.

--- src/Data/AboutData.jsx
+++ src/Data/AboutData.jsx
@@ -1,22 +1,22 @@
 export const AboutData = [
     {
-        title: "Lorem Ipsum 1",
-        body: " Horem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec",
+        title: "Interaction with VR",
+        body: "In this experiment, participants engage with a virtual reality environment designed to simulate real-life scenarios. Their behavioral and emotional responses are observed to understand how immersive experiences influence perception and decision-making.",
         imgSrc: "./assets/images/intro1.jpeg"
     }, 
     {
-        title: "Lorem Ipsum 2",
-        body: " Horem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec",
+        title: "EEG Recordings of Human Action",
+        body: "Under the Transdisciplinary Research (TDR) Lab, neuroscience meets design thinking. Here, an experiment records EEG signals from participants as they perform tasks and explore neural dynamics underlying habit and behavior formation. EEG Signal charts are analyzed to identify specific brainwave patterns and their changes during task engagement, providing insights into how brain activity correlated with behavioral responses and cognitive processes.",
         imgSrc: "./assets/images/intro2.jpg"
     },
     {
-        title: "Lorem Ipsum 3",
-        body: " Horem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec",
+        title: "Real-Time Brain Activity Monitoring with Neuroscience Headband in the TDR Lab",
+        body: "In the TDR Lab neuroscience experiment, participants wear a headband device that monitors brain activity in real time. This setup enables the lab to study neural responses during cognitive and behavioral tasks, integrating transdisciplinary methods to better understand brain-function relationships.",
         imgSrc: "./assets/images/intro3.jpeg"
     },
     {
-        title: "Lorem Ipsum 4",
-        body: " Horem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec",
+        title: "Understanding Neural Mechanism of Weaving",
+        body: "In the TDR Lab experiment, a weaver participant wears an innovative EEG headband that monitor brain activity during weaving tasks. This wearable device captures neural signals in real time, allowing the study of cognitive and motor processes involved in the artisanal craft.",
         imgSrc: "./assets/images/intro4.jpg"
     },
 ]
--- src/components/AboutPageTiles/index.jsx
+++ src/components/AboutPageTiles/index.jsx
@@ -12,7 +12,8 @@ export const AboutPageIntro = () => {
                 A Trans Disciplinary Research Lab
             </div>
             <div className="aboutPageIntroText">
-                Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitu
+                The Transdisciplinary research (TDR) lab is dedicated to exploring the complex connections between habit and behavior. The lab brings the creativity of students in their respective domains and find applications into the areas of Behavioral Sciences.
+                Projects specifically focus on understanding behavioral patterns, their psychological and environmental influences, and strategies to promote change. The TDR-lab aims to integrate the perspectives from design, psychology, technology, and social sciences. 
             </div>
         </div>
     );
